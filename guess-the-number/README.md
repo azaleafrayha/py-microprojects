9fad29fe0aacb496a4fde415be5e7958d51e51d6
azaleafrayha <zalea473@gmail.com>
2026-02-20 11:24:49 +0700

    added error handling and tweaks

--- guess-the-number/README.md
+++ guess-the-number/README.md
@@ -1,1 +1,2 @@
-WIP
+# WIP
+Type `python guess-the-number/main.py` to run the file.
--- guess-the-number/main.py
+++ guess-the-number/main.py
@@ -1,22 +1,31 @@
 import random
 target = random.randint(1, 10) # putting it outside the loop so it only generates once per game (not every time the user wants to play again)
+print(target)
 
 print("Welcome to the Number Guessing Game!")
 print("I have selected a random number between 1 and 10. Can you guess it?")
 
 while True:
-    n = int(input("Enter your guess: ")) #TODO: ADD ERROR HANDLING   
-    
+    try:
+        n = int(input("Enter your guess: "))   
+    except ValueError:
+        print("Write down a number between 1 to 10, you dummy! Try again.")
+        continue
+
     if n == target: # check the correct one first, then the wrong ones
-        print("Aw you guessed it! Wanna try again?")            
+        print("WHOA YOU GUESSED IT RIGHT! Wanna try again?")            
     elif n < target:
         print("Haha, too low! Try again if you dare.")        
     elif n > target:
         print("Damn that's too high! Wanna give it another shot?")
 
-    play_again = input("Press Y to play again: ")
+    play_again = input("Press Y to play again, N to start a new game, and any keys to quit: ")
     if play_again.strip() == 'Y' or play_again.strip() == 'y':
-        continue 
+        continue
+    elif play_again.strip() == 'N' or play_again.strip() == 'n':
+        target = random.randint(1, 10) # generate a new number for the new game
+        print("Alright, a new number has been selected. Let's see if you can guess it!")
+        print(target) 
     else:
-        print("Alright thanks for playing! Goodbye!")
+        print("Oh you wanna quit? Fine, thanks for playing! Goodbye!")
         break
